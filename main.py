11768da644c9301e821f93f578777cea701798b0
VictorDelfour <108739125+VictorDelfour@users.noreply.github.com>
2022-08-03 10:56:04 -0400

    Création des uplets et linkage trouvés
    Les noms des fichiers sont de type 01linked.csv
    
    Chaque chiffre correspond à un type de donnée
    0 : Prénom
    1 : Nom
    2 : Date de naissance
    3 : Mail
    4 : Téléphone
    5 : Addresse
    6 : État
    
    S'il y a 2 chiffres, c'est un doublet, 3 un triplet,...
    
    Le programme prend moins de 10 min à tourner.

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 import time
 import pandas as pd
 from bloom_filter import BloomFilter
-
+import hashlib
+import numpy
 
 #tic_A = time.perf_counter()
 
-dataset_A = pd.read_csv("dataA_hash_hs.csv")
+dataset_A = pd.read_csv("dataA_hash.csv", sep=';')
 dataset_A = dataset_A.reset_index()
-""""
-bloom_filter = BloomFilter(max_elements=2000000, error_rate=0.000001)
 
-for index, row in dataset_A.iterrows():
-    if not pd.isnull(row["SSN"]):
-        bloom_filter.add(row["SSN"])
-
-
-toc_A = time.perf_counter()
-print("A took : ", toc_A-tic_A, "s")
-
-tic_B = time.perf_counter()
-"""
-dataset_B = pd.read_csv("dataB_hash_hs.csv")
+dataset_B = pd.read_csv("dataB_hash.csv", sep=';')
 dataset_B = dataset_B.reset_index()
 
-""""
-ids = 0
-for index, row in dataset_B.iterrows():
-    if not pd.isnull(row["SSN"]):
-        try:
-            assert row["SSN"] not in bloom_filter
-        except AssertionError:
-            ids = ids + 1
-
-print("Number of identified elements ", ids)
-
-toc_B = time.perf_counter()
-print("B took : ", toc_B-tic_B, "s")
-"""
 fNameA = []
 lNameA = []
 bDayA = []
@@ -44,6 +19,7 @@ mailA = []
 phoneA = []
 addressA = []
 stateA = []
+SSNA = []
 
 #%%
 dataset_A = dataset_A.reset_index()
@@ -55,6 +31,7 @@ for index, row in dataset_A.iterrows():
     phoneA.append(row["Phone"])
     addressA.append(row["Address"])
     stateA.append(row["State"])
+    SSNA.append(row["SSN"])
 
 fNameB = []
 lNameB = []
@@ -63,6 +40,7 @@ mailB = []
 phoneB = []
 addressB = []
 stateB = []
+SSNB = []
 
 dataset_B = dataset_B.reset_index()
 for index, row in dataset_B.iterrows():
@@ -73,145 +51,184 @@ for index, row in dataset_B.iterrows():
     phoneB.append(row["Phone"])
     addressB.append(row["Address"])
     stateB.append(row["State"])
+    SSNB.append(row["SSN"])
+registreA = [fNameA, lNameA, bDayA, mailA, phoneA, addressA, stateA, SSNA]
+registreB = [fNameB, lNameB, bDayB, mailB, phoneB, addressB, stateB, SSNB]
 
-registreA = [fNameA, lNameA, bDayA, mailA, phoneA, addressA, stateA]
-registreB = [fNameB, lNameB, bDayB, mailB, phoneB, addressB, stateB]
-
+upletA = []
+upletB = []
+idA = []
+idB = []
 
 for i in range(0, 7, 1): #2-uplets
     for j in range(i+1, 7, 1):
         print("i=", i, "j=", j)
-        lNum = 0
         upletA = []
         upletB = []
         idA = []
         idB = []
+
         for k in range(len(fNameA)):
-            if registreA[i][k] == "" or registreA[j][k] == "":
+            if registreA[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                 upletA.append("")
             else:
-                upletA.append(registreA[i][k]+registreA[j][k]) #hasher directement ici
-        print("upletA créé")
+                upletA.append(hashlib.sha256((registreA[i][k]+registreA[j][k]).encode('utf-8')).hexdigest()) #hasher directement ico
+
+        indexA = numpy.argsort(upletA)
+        upletA = numpy.sort(upletA)
         for k in range(len(fNameB)):
-            if registreB[i][k] == "" or registreB[j][k] == "":
+            if registreB[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                 upletB.append("")
             else:
-             upletB.append(registreB[i][k]+registreB[j][k]) #hasher directement ici
-        print("upletB créé")
+             upletB.append(hashlib.sha256((registreB[i][k]+registreB[j][k]).encode('utf-8')).hexdigest()) #hasher directement ici
+        tic_A = time.perf_counter()
+        indexB = numpy.argsort(upletB)
+        upletB = numpy.sort(upletB)
+
+        l = 0
+        tic_A = time.perf_counter()
         for k in range(len(upletA)):
             if not upletA[k] == "":
-                for l in range(len(upletB)):
-                    if upletA[k] == upletA[l]:
-                        idA.append(k)
-                        idB.append(l)
-            lNum += 1
-            print(lNum)
+                while l < 500000 and upletA[k] > upletB[l]:
+                    l += 1
+                if l < 500000 and upletA[k] == upletB[l]:
+                    idA.append(indexA[k]+2)
+                    idB.append(indexB[l]+2)
+                    l += 1
+        toc_A = time.perf_counter()
+        print("temps tri : ", toc_A-tic_A)
         C = {'idA': idA, 'idB': idB}
-        donnees = pd.DataFrame(C,columns=['idA','idB'])
-        export_csv = pd.dt.to_csv(str(i)+str(j)+'linked.csv', index=None, header=True, encoding='utf-8', sep=';')
-        print(donnees)
+        donnees = pd.DataFrame(C, columns=['idA', 'idB'])
+        donnees.to_csv(str(i)+str(j)+'linked.csv', index=True, header=True, encoding='utf-8', sep=';')
+        print("export fait")
+
 
 for i in range(0, 7, 1): #3-uplets
     for j in range(i+1, 7, 1):
         for a in range(j+1, 7, 1):
-            print("i=", i, "j=", j, "a", a)
-            lNum = 0
+            print("i=", i, "j=", j, "a=", a)
             upletA = []
             upletB = []
             idA = []
             idB = []
+
             for k in range(len(fNameA)):
-                if registreA[i][k] == "" or registreA[j][k] == "" or registreA[a][k] == "":
+                if registreA[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                     upletA.append("")
                 else:
-                    upletA.append(registreA[i][k]+registreA[j][k]+registreA[a][k]) #hasher directement ici
-            print("upletA créé")
+                    upletA.append(hashlib.sha256((registreA[i][k]+registreA[j][k]+registreA[a][k]).encode('utf-8')).hexdigest()) #hasher directement ico
+
+            indexA = numpy.argsort(upletA)
+            upletA = numpy.sort(upletA)
             for k in range(len(fNameB)):
-                if registreB[i][k] == "" or registreB[j][k] == "" or registreB[a][k] == "":
+                if registreB[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                     upletB.append("")
                 else:
-                    upletB.append(registreB[i][k]+registreB[j][k]+registreB[a][k]) #hasher directement ici
-            print("upletB créé")
+                    upletB.append(hashlib.sha256((registreB[i][k]+registreB[j][k]+registreB[a][k]).encode('utf-8')).hexdigest()) #hasher directement ici
+            tic_A = time.perf_counter()
+            indexB = numpy.argsort(upletB)
+            upletB = numpy.sort(upletB)
+            l = 0
+            tic_A = time.perf_counter()
             for k in range(len(upletA)):
                 if not upletA[k] == "":
-                    for l in range(len(upletB)):
-                        if upletA[k] == upletA[l]:
-                            idA.append(k)
-                            idB.append(l)
-                lNum += 1
-                print(lNum)
+                    while l < 500000 and upletA[k] > upletB[l]:
+                        l += 1
+                    if l < 500000 and upletA[k] == upletB[l]:
+                        idA.append(indexA[k]+2)
+                        idB.append(indexB[l]+2)
+                        l += 1
+            toc_A = time.perf_counter()
+            print("temps tri : ", toc_A-tic_A)
             C = {'idA': idA, 'idB': idB}
             donnees = pd.DataFrame(C, columns=['idA', 'idB'])
-            export_csv = pd.dt.to_csv(str(i)+str(j)+str(a)+'linked.csv', index=None, header=True, encoding='utf-8', sep=';')
-            print(donnees)
+            donnees.to_csv(str(i)+str(j)+str(a)+'linked.csv', index=True, header=True, encoding='utf-8', sep=';')
+            print("export fait")
+
 
 for i in range(0, 7, 1): #4-uplets
     for j in range(i+1, 7, 1):
         for a in range(j+1, 7, 1):
             for b in range(a+1, 7, 1):
-                print("i=", i, "j=", j, "a", a, "b", b)
-                lNum = 0
+                print("i=", i, "j=", j, "a=", a, "b=", b)
                 upletA = []
                 upletB = []
                 idA = []
                 idB = []
+
                 for k in range(len(fNameA)):
-                    if registreA[i][k] == "" or registreA[j][k] == "" or registreA[a][k] == "" or registreA[b][k] == "":
+                    if registreA[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[b][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                         upletA.append("")
                     else:
-                        upletA.append(registreA[i][k]+registreA[j][k]+registreA[a][k]+registreA[b][k]) #hasher directement ici
-                print("upletA créé")
+                        upletA.append(hashlib.sha256((registreA[i][k]+registreA[j][k]+registreA[a][k]+registreA[b][k]).encode('utf-8')).hexdigest()) #hasher directement ico
+
+                indexA = numpy.argsort(upletA)
+                upletA = numpy.sort(upletA)
                 for k in range(len(fNameB)):
-                    if registreB[i][k] == "" or registreB[j][k] == "" or registreB[a][k] == "" or registreB[b][k] == "":
+                    if registreB[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[b][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                         upletB.append("")
                     else:
-                        upletB.append(registreB[i][k]+registreB[j][k]+registreB[a][k]+registreB[b][k]) #hasher directement ici
-                print("upletB créé")
+                        upletB.append(hashlib.sha256((registreB[i][k]+registreB[j][k]+registreB[a][k]+registreB[b][k]).encode('utf-8')).hexdigest()) #hasher directement ici
+                tic_A = time.perf_counter()
+                indexB = numpy.argsort(upletB)
+                upletB = numpy.sort(upletB)
+                l = 0
+                tic_A = time.perf_counter()
                 for k in range(len(upletA)):
                     if not upletA[k] == "":
-                        for l in range(len(upletB)):
-                            if upletA[k] == upletA[l]:
-                                idA.append(k)
-                                idB.append(l)
-                    lNum += 1
-                    print(lNum)
+                        while l < 500000 and upletA[k] > upletB[l]:
+                            l += 1
+                        if l < 500000 and upletA[k] == upletB[l]:
+                            idA.append(indexA[k]+2)
+                            idB.append(indexB[l]+2)
+                            l += 1
+                toc_A = time.perf_counter()
+                print("temps tri : ", toc_A-tic_A)
                 C = {'idA': idA, 'idB': idB}
-                donnees = pd.DataFrame(C,columns=['idA', 'idB'])
-                export_csv = pd.dt.to_csv(str(i)+str(j)+str(a)+str(b)+'linked.csv', index=None, header=True, encoding='utf-8', sep=';')
-                print(donnees)
+                donnees = pd.DataFrame(C, columns=['idA', 'idB'])
+                donnees.to_csv(str(i)+str(j)+str(a)+str(b)+'linked.csv', index=True, header=True, encoding='utf-8', sep=';')
+                print("export fait")
 
 for i in range(0, 7, 1): #5-uplets
     for j in range(i+1, 7, 1):
         for a in range(j+1, 7, 1):
             for b in range(a+1, 7, 1):
                 for c in range(b+1, 7, 1):
-                    print("i=", i, "j=", j, "a", a, "b", b, "c", c)
-                    lNum = 0
+                    print("i=", i, "j=", j, "a=", a, "b=", b, "c=", c)
                     upletA = []
                     upletB = []
                     idA = []
                     idB = []
+
                     for k in range(len(fNameA)):
-                        if registreA[i][k] == "" or registreA[j][k] == "" or registreA[a][k] == "" or registreA[b][k] == "" or registreA[c][k] == "":
+                        if registreA[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[b][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreA[c][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                             upletA.append("")
                         else:
-                            upletA.append(registreA[i][k]+registreA[j][k]+registreA[a][k]+registreA[b][k]+registreA[c][k]) #hasher directement ici
-                    print("upletA créé")
+                            upletA.append(hashlib.sha256((registreA[i][k]+registreA[j][k]+registreA[a][k]+registreA[b][k]+registreA[c][k]).encode('utf-8')).hexdigest()) #hasher directement ico
+
+                    indexA = numpy.argsort(upletA)
+                    upletA = numpy.sort(upletA)
                     for k in range(len(fNameB)):
-                        if registreB[i][k] == "" or registreB[j][k] == "" or registreB[a][k] == "" or registreB[b][k] == "" or registreB[c][k] == "":
+                        if registreB[i][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[j][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[a][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[b][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' or registreB[c][k] == 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855':
                             upletB.append("")
                         else:
-                            upletB.append(registreB[i][k]+registreB[j][k]+registreB[a][k]+registreB[b][k]+registreB[c][k]) #hasher directement ici
-                    print("upletB créé")
+                            upletB.append(hashlib.sha256((registreB[i][k]+registreB[j][k]+registreB[a][k]+registreB[b][k]+registreB[c][k]).encode('utf-8')).hexdigest()) #hasher directement ici
+                    tic_A = time.perf_counter()
+                    indexB = numpy.argsort(upletB)
+                    upletB = numpy.sort(upletB)
+                    l = 0
+                    tic_A = time.perf_counter()
                     for k in range(len(upletA)):
                         if not upletA[k] == "":
-                            for l in range(len(upletB)):
-                                if upletA[k] == upletA[l]:
-                                    idA.append(k)
-                                    idB.append(l)
-                        lNum += 1
-                        print(lNum)
+                            while l < 500000 and upletA[k] > upletB[l]:
+                                l += 1
+                            if l < 500000 and upletA[k] == upletB[l]:
+                                idA.append(indexA[k]+2)
+                                idB.append(indexB[l]+2)
+                                l += 1
+                    toc_A = time.perf_counter()
+                    print("temps tri : ", toc_A-tic_A)
                     C = {'idA': idA, 'idB': idB}
                     donnees = pd.DataFrame(C, columns=['idA', 'idB'])
-                    export_csv = pd.dt.to_csv(str(i)+str(j)+str(a)+str(b)+str(c)+'linked.csv', index=None, header=True, encoding='utf-8', sep=';')
-                    print(donnees) 
+                    donnees.to_csv(str(i)+str(j)+str(a)+str(b)+str(c)+'linked.csv', index=True, header=True, encoding='utf-8', sep=';')
+                    print("export fait")
